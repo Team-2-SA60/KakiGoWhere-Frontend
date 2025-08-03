e386bafffa8d8a70975077eb3a13b1678b2a23a7
im-ksc <chan_kin_seng@hotmail.com>
2025-08-03 22:33:20 +0800

    fix: added buildtime env for API base URL

--- app/src/utils/axios.jsx
+++ app/src/utils/axios.jsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+// const apiUrl = 'http://localhost:8080';
+
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: `${apiUrl}/api`,
   withCredentials: true,
 });
 
