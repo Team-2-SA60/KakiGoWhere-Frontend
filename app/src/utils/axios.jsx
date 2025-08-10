05c1264cdb179d7d3f3250e33a46ff3b10fdb953
im-ksc <chan_kin_seng@hotmail.com>
2025-08-10 17:12:41 +0800

    chore: converge API_URL and ML_API_URL

--- app/src/utils/axios.jsx
+++ app/src/utils/axios.jsx
@@ -1,18 +1,16 @@
 import axios from 'axios';
 
 const apiUrl = import.meta.env.VITE_API_URL;
-// const apiUrl = 'http://localhost:8080';
-const mlApiUrl = import.meta.env.VITE_ML_API_URL;
 
 // Java backend client
 const api = axios.create({
-  baseURL: `${apiUrl}/api`,
+  baseURL: `${apiUrl}:8080/api`,
   withCredentials: true,
 });
 
 // Flask ML service client
 export const mlApi = axios.create({
-  baseURL: mlApiUrl,
+  baseURL: `${apiUrl}:5001/api`,
 });
 
 export default api;
